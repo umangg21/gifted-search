c67a4a6137aae9faff667a0ead1592901a432002
Umang <umang@indwealth.in>
2019-09-01 14:52:32 +0530

    Fix for Mozila

--- src/components/common/Toggle.jsx
+++ src/components/common/Toggle.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 
-const Toggle = (props) => <div onClick={(e) => {
-    if (e.target.nodeName === "SPAN")
-        props.onToggle(!props.isChecked)
-}} {...props}>
-    <label htmlFor="togglebox" className="switch-toggle">
-        <input id="togglebox" type="checkbox" checked={props.isChecked}
-        />
-        <span className={`slider-toggle round-toggle`} />
-    </label>
-</div>
+const Toggle = (props) => {
+    return (
+        <div onClick={(e) => {
+            if (e.target.nodeName === "SPAN")
+                props.onToggle(!props.isChecked)
+        }}>
+            <label htmlFor="togglebox" className="switch-toggle">
+                <input id="togglebox" type="checkbox" checked={props.isChecked}
+                />
+                <span className={`slider-toggle round-toggle`} />
+            </label>
+        </div>
+    )
+}
 
 
 export default React.memo(Toggle);
