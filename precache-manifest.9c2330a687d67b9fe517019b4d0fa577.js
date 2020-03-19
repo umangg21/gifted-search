bccc0701f0bbf8653ce5e053f15239bae051c020
Umang <umang@indwealth.in>
2020-03-19 20:47:50 +0530

    Updates

---
+++ precache-manifest.9c2330a687d67b9fe517019b4d0fa577.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52be3e01c92bc97fa544ee787245f8dc",
+    "url": "/gifted-search/index.html"
+  },
+  {
+    "revision": "531eeeb0cac9ceff3b93",
+    "url": "/gifted-search/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "07be21f7295264b017c7",
+    "url": "/gifted-search/static/js/2.313c1519.chunk.js"
+  },
+  {
+    "revision": "531eeeb0cac9ceff3b93",
+    "url": "/gifted-search/static/js/main.5f4d8e5f.chunk.js"
+  },
+  {
+    "revision": "135cdd4bb2cbcb6cc54f",
+    "url": "/gifted-search/static/js/runtime~main.e3705e99.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gifted-search/static/media/logo.5d5d9eef.svg"
+  }
+]);
